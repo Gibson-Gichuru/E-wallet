ddc86acef03ea8d28e9a0923a91ecc9f4b6933f8
Gibson-Gichuru <gichuru.developer@gmail.com>
2023-01-24 09:12:31 +0300

    Test Suite Setup
    The application tests if the application is working with the Testing class config

---
+++ tests/__init__.py
@@ -0,0 +1,20 @@
+import unittest
+
+from app import create_app
+
+class BaseTestConfig(unittest.TestCase):
+
+    def setUp(self) -> None:
+        
+        self.app = create_app("testing")
+
+        self.app_context = self.app.app_context()
+
+        self.app_context.push()
+
+
+    def tearDown(self) -> None:
+        
+        self.app_context.pop()
+
+        self.app = None
---
+++ tests/test_app_config.py
@@ -0,0 +1,7 @@
+from tests import BaseTestConfig
+
+class TestAppConfig(BaseTestConfig):
+
+    def test_app_is_in_test_mode(self) -> None:
+
+        self.assertTrue(self.app.config["TESTING"])
