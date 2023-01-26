3b16ba8ee321622dc87b93d2505fdd70d32fbd9a
Gibson-Gichuru <gichuru.developer@gmail.com>
2023-01-26 11:27:27 +0300

    Changing default server Error response from HTML to application/json

--- manage.py
+++ manage.py
@@ -1,9 +1,27 @@
 import os
 import sys
+import json
 from app import create_app
+from app.models import User, Account, Task, Payment, Status, Actions
+from werkzeug.exceptions import HTTPException
 
 app = create_app(os.getenv("CONFIG") or "default")
 
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+
+    response = e.get_response()
+
+    response.data = json.dumps({
+        "code":e.code,
+        "name":e.name,
+        "description":e.description
+    })
+
+    response.content_type = "application/json"
+
+    return response
 
 @app.cli.command()
 def test():
