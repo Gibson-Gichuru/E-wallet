e0733c7cfea1e18416f03d8a5fd5889865b3a6e3
Gibson-Gichuru <gichuru.developer@gmail.com>
2023-01-26 11:21:29 +0300

    Database migration history

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 import os
 
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
 
     SECRETE_KEY = os.getenv("SECRETE_KEY")
-
+     
 
 class Development(Config):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or \
+    "sqlite:///" + os.path.join(base_dir, "dev-data.sqlite")
 
     @staticmethod
     def init_app(app):
@@ -17,6 +22,9 @@ class Development(Config):
 class Testing(Config):
 
     TESTING = True
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or \
+    "sqlite:///" + os.path.join(base_dir, "test-data.sqlite")
 
     @staticmethod
     def init_app(app):
