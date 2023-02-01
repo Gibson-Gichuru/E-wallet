59a7682edb71cd967caaf02263744bfb94136d1f
Gibson-Gichuru <gichuru.developer@gmail.com>
2023-02-01 12:53:54 +0300

    ussid options response logic update

--- app/ussid/views.py
+++ app/ussid/views.py
@@ -3,8 +3,7 @@ from flask import request
 from config import base_dir
 import os
 import json
-import re
-from app.models import User
+from app.models import User, Actions, Status
 
 
 class UssidCallback(MethodView):
@@ -16,10 +15,105 @@ class UssidCallback(MethodView):
 
             self.menu_text = json.load(file)
 
+    def process_input(self, text):
+
+        return (len(text.split("*")), text.split("*"))
+
+    def process_level_1_menu_option_nill(self, user):
+
+        if user:
+
+            if user.account.status.default:
+
+                return self.menu_text.get("activate")
+
+            if user.account.can(Actions.TRANSACT):
+
+                return self.menu_text.get("main")
+
+            return self.menu_text.get("suspended")
+
+        return self.menu_text.get("welcome")
+
+    def process_level_1_menu_option_1(self, user):
+
+        if user:
+
+            if user.account.can(Actions.TRANSACT):
+
+                return self.menu_text.get("top_up")
+
+            return self.menu_text.get("activate_accept")
+
+        return self.menu_text.get("register")
+
+    def process_level_1_menu_option_2(self, user):
+
+        if user:
+
+            if user.account.can(Actions.TRANSACT):
+
+                return self.menu_text.get("withdraw")
+
+            return self.menu_text.get("activate_reject")
+
+        return self.menu_text.get("cancel")
+
+
+    def process_level_1_menu_option_3(self, user):
+
+        balance = float(user.account.balance)
+
+        return self.menu_text.get(
+            "check_balance"
+        ) + "{}".format(balance)
+
+
+    def process_level_1_menu_option_4(self, user):
+
+        # Todo implement statement processing
+
+        return self.menu_text.get("statement") 
+
+    def process_level_1_menu_option_5(self, user):
+
+        new_status = Status.query.filter_by(
+            status_name="Deactivated"
+        ).first()
+
+        user.account.status = new_status
+
+        user.update()
+
+        return self.menu_text.get("deactivate")
+
+    def process_level_2_menu_option_nill(self,text, phone_number):
+
+        user_name = text[-1][-1]
+
+        new_user = User(
+            username=user_name,
+            phonenumber=phone_number.replace("+", "")
+        )
+
+        new_user.add(new_user)
+
+        return self.menu_text.get("reg_success")
+
+    def process_level_2_menu_option_1(self, user):
+
+        # Todo topup process
+
+        return self.menu_text.get("topup_success")
+
+    def process_level_2_menu_option_2(self, user):
+
+        # Todo withdraw process
+
+        return self.menu_text.get("withdraw_success")
+
     def post(self):
 
-        session_id = request.values.get("sessionId", None)
-        session_code = request.values.get("sessionCode", None)
         phone_number = request.values.get("phoneNumber", None)
         text = request.values.get("text", "default")
 
@@ -27,32 +121,56 @@ class UssidCallback(MethodView):
             phonenumber=phone_number
         ).first()
 
-        if user is None or user.account.status.status_name != "Active":
-
-            # check if this is the start of the session
-
+        if self.process_input(text)[0] == 1:
+            # First Menu
             if text == "":
 
-                return self.menu_text.get("welcome")
+                return self.process_level_1_menu_option_nill(user)
 
             elif text == "1":
 
-                return self.menu_text.get("register")
+                return self.process_level_1_menu_option_1(user)
 
             elif text == "2":
 
-                return self.menu_text.get("cancel")
+                return self.process_level_1_menu_option_2(user)
+
+            elif text == "3":
+
+                return self.process_level_1_menu_option_3(user)
+
+            elif text == "4":
+
+                return self.process_level_1_menu_option_4(user)
+
+            elif text == "5":
+
+                return self.process_level_1_menu_option_5(user)
 
             else:
-                
-                name = "".join(re.findall("[a-zA-Z]",text))
 
-                user = User(username=name, phonenumber=phone_number)
+                return self.menu_text.get("cancel")
 
-                user.add(user)
+        if self.process_input(text=text)[0] == 2:
 
-                return self.menu_text.get("reg_success")
+            fine_text = self.process_input(text=text)
 
-        else:
+            if user is None:
 
-            return self.menu_text.get("main")
+                return self.process_level_2_menu_option_nill(
+                    text=fine_text,
+                    phone_number=phone_number
+                )
+
+
+            if user.account.can(Actions.TRANSACT):
+
+                if fine_text[-1][0] == "1":
+
+                    return self.process_level_2_menu_option_1(user)
+
+                return self.process_level_2_menu_option_2(user)
+
+            return self.menu_text.get("cancel")
+
+        return self.menu_text.get("invalid option")
