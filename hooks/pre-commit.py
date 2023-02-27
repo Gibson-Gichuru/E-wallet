17f12372abb802055e12303e5067f263f0cf5d79
Gibson-Gichuru <gichuru.developer@gmail.com>
2023-02-27 16:30:51 +0300

    Ignoring Lint error E731

--- hooks/pre-commit.py
+++ hooks/pre-commit.py
@@ -18,7 +18,7 @@ lint_command_one = f"flake8 {base_dir} " + \
 lint_command_two = f"flake8 {base_dir} " + \
 "--count --max-complexity=10" + \
 " --max-line-length=100 " + \
-"--ignore C901,E121,E221,E122,E126,E231,E123,F841,W503,W293,W504 " + \
+"--ignore C901,E121,E221,E122,E126,E231,E123,E731,F841,W503,W293,W504 " + \
 "--exclude __pycache__,migrations,env --show-source --statistics"
 
 
