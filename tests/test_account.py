605e369f507fa06c3ecfb02fa6d9764edff14a01
Gibson-Gichuru <gichuru.developer@gmail.com>
2023-03-10 16:32:21 +0300

    account balance test update

--- tests/test_account.py
+++ tests/test_account.py
@@ -14,9 +14,17 @@ class TestAccount(BaseTestConfig):
 
         self.user.add(self.user)
 
+    @mock.patch("app.models.datetime", autospec=True)
     @mock.patch("app.models.send_sms", autospec=True)
     @mock.patch("app.models.Task.schedule", autospec=True)
-    def test_balance_update_notification(self,schedule, msg):
+    def test_balance_update_notification(self,schedule, msg, date_mock):
+
+        date_mock.strftime.return_value = "test"
+
+        data = {
+            "balance":self.user.account.balance.to_eng_string(),
+            "date":date_mock.strftime()
+        }
 
         Account.update_balance(
             transaction_type="credit",
@@ -29,7 +37,9 @@ class TestAccount(BaseTestConfig):
             description="Balance Notification",
             target_func=msg,
             queue=self.app.queue,
-            template="BALANCE"
+            template="BALANCE",
+            data=data,
+            recipient=f"+{self.user.phonenumber}"
         )
 
     @skip("No implemented yet")
