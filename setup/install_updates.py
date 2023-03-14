ea1037f37d9571539f62a936b4effc7ef601fbec
Gibson-Gichuru <gichuru.developer@gmail.com>
2023-03-14 14:09:02 +0300

    install updates

--- setup/install_updates.py
+++ setup/install_updates.py
@@ -1,11 +1,9 @@
 import subprocess
-from . import poetry_env
 
 
 def install_updates():
 
     subprocess.run(
         "poetry install --no-root".split(),
-        env=poetry_env,
         check=True
     )
